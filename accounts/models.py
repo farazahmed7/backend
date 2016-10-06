ece9019020ae9d3e4b95571c0036b56e22a4749b
faraz <faraz.dipsite@gmail.com>
2016-10-06 15:30:59 +0530

    indenation fixed

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 
-
 from django.db import models
 
 # Create your models here.
@@ -10,16 +9,15 @@ from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    username=models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100)
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    upvote=models.IntegerField
-    post=models.CharField(max_length=100)
-    image=models.ImageField(upload_to='post')
-    def __str__(self):
-		return self.post
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    upvote = models.IntegerField
+    post = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post')
 
-    
+    def __str__(self):
+        return self.post
