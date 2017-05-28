3927308d8cb7c963099253ad1787f6a6d140566e
faraz <faraz.dipsite@gmail.com>
2017-05-28 17:51:43 +0530

    fixed

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from allauth.socialaccount.models import SocialAccount
 from django.contrib.auth.models import User
 from stream_django.activity import Activity
 
@@ -13,10 +14,9 @@ class Post(models.Model):
     def __str__(self):
         return self.post
 
-class UserProfile:
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    username=user.username
+class UserProfile(models.Model):
+    user = models.ForeignKey(SocialAccount)
     dp=models.CharField(max_length=100)
 
     def __str__(self):
-        return self.user.username
+        return self.user.user.username
